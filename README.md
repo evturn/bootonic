7492b8ca42f48a8f6d7a55328efd1cb56220f89d
Evan Turner <evturn@gmail.com>
2017-09-22 23:38:01 -0400

    Init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+<img src="https://www.dropbox.com/s/7pax8utugwnj2r8/favicon.png?dl=1">
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="root">
+        <div className="header">
+          <img 
+            src="https://www.dropbox.com/s/7pax8utugwnj2r8/favicon.png?dl=1"
+            className="logo" 
+            alt="Bootonic logo" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
