f794278c9f7fde2a4c7354c71dd44944934dcb58
Evan Turner <ev@evturn.com>
2018-04-02 22:23:22 -0400

    Provide additional static endpoint

--- index.mjs
+++ index.mjs
@@ -4,6 +4,7 @@ import path from 'path';
 const app = express();
 
 app.use(express.static(path.join(process.cwd(), 'build')));
+app.use('/src', express.static(path.join(process.cwd(), 'src', 'static')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'build', 'index.html'));
